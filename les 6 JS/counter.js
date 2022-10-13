9d8fd0384a69b908b17492fc54635faa33e9493a
Zgerskih <114507567+Zgerskih@users.noreply.github.com>
2022-10-13 22:17:00 +0300

    TODO

---
+++ les 6 JS/counter.js
@@ -0,0 +1,11 @@
+const dominputTodoTitle = document.getElementById("inputTodoTitle");
+const dombtnCreateTodo = document.getElementById("ibtnCreateTodo");
+const domlistOfTodo = document.getElementById("listOfTodo");
+
+
+console.log(
+    ">dominputTodoTitle",
+    domInputTodoTitle,
+    domIbtnCreateTodo,
+    domListOfTodo
+);
