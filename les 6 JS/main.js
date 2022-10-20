8706a8f6dfdc611cc4578bb9dcaf82c455b3001f
Zgerskih <114507567+Zgerskih@users.noreply.github.com>
2022-10-20 22:22:18 +0300

    les 11

--- les 6 JS/main.js
+++ les 6 JS/main.js
@@ -1,4 +1,8 @@
 import TodoVO from './src/model/vos/TodoVO.js';
+import {disableButtonWhenTextInvalid} from "./src/model/utils/domUtils.js";
+import {isStringNotNumberAndNotEmpty} from "./src/model/utils/StringUtils.js";
+import {localStorageListOf, localStorageSaveListOfWithKey} from "./src/model/utils/databaseUtils.js";
+import TodoView from "./src/wiew/TodoView.js";
 
 const domInpTodoTitle = document.getElementById('inpTodoTitle');
 const domBtnCreateTodo = document.getElementById('btnCreateTodo');
@@ -6,6 +10,7 @@ const domListOfTodos = document.getElementById('listOfTodos');
 
 domBtnCreateTodo.addEventListener('click', onBtnCreateTodoClick);
 domInpTodoTitle.addEventListener('keyup', onInpTodoTitleKeyup);
+domListOfTodos.addEventListener('change', onTodoListChange);
 
 const LOCAL_LIST_OF_TODOS = 'listOfTodos';
 
@@ -14,88 +19,49 @@ const listOfTodos = localStorageListOf(LOCAL_LIST_OF_TODOS);
 console.log('> Initial value -> listOfTodos', listOfTodos);
 
 renderTodoListInContainer(listOfTodos, domListOfTodos);
-disableButtonWhenTextInvalid(
-  domBtnCreateTodo,
-  domInpTodoTitle.value,
-  isStringNotNumberAndNotEmpty,
-  {
-    textWhenEnabled: 'Create',
-    textWhenDisabled: 'Enter text',
-  }
-);
+disableButtonWhenTextInvalid( domBtnCreateTodo, domInpTodoTitle.value, isStringNotNumberAndNotEmpty, );
 
+function onTodoListChange(event) {
+  console.log('> onTodoListChange -> event:', event);
+  const target = event.target;
+  const index = target.id;
+  if (index && typeof index === 'string'){
+    const indexInt = parseInt(index.trim())
+    const todoVO = listOfTodos[index];
+    console.log('> onTodoListChange -> todoVO:', todoVO);
+    console.log('> onTodoListChange -> index:', index);
+    todoVO.isCompleted = !!target.checked;
+    saveListOfTodo();
+      }
+  }
 function onBtnCreateTodoClick() {
   // console.log('> domBtnCreateTodo -> handle(click)', event);
   const todoTitleValueFromDomInput = domInpTodoTitle.value;
   // console.log('> domBtnCreateTodo -> todoInputTitleValue:', todoTitleValueFromDomInput);
   if (isStringNotNumberAndNotEmpty(todoTitleValueFromDomInput)) {
     listOfTodos.push(TodoVO.createFromTitle(todoTitleValueFromDomInput));
-    localStorage.setItem(LOCAL_LIST_OF_TODOS, JSON.stringify(listOfTodos));
+    saveListOfTodo();
     renderTodoListInContainer(listOfTodos, domListOfTodos);
+    domInpTodoTitle.value = '';
   }
 }
-
 function onInpTodoTitleKeyup(event) {
   // console.log('> onInpTodoTitleKeyup:', event);
   const inputValue = event.currentTarget.value;
   // console.log('> onInpTodoTitleKeyup:', inputValue);
-  disableButtonWhenTextInvalid(
-    domBtnCreateTodo,
-    inputValue,
-    isStringNotNumberAndNotEmpty,
-    {
-      textWhenEnabled: 'Create',
-      textWhenDisabled: 'Enter text',
-    }
-  );
+  disableButtonWhenTextInvalid( domBtnCreateTodo, inputValue, isStringNotNumberAndNotEmpty, );
 }
-
-function isStringNotNumberAndNotEmpty(value) {
-  const isValueString = typeof value === 'string';
-  const isValueNotNumber = isNaN(parseInt(value));
-
-  const result = isValueString && isValueNotNumber && value.length > 0;
-
-  console.log('> isStringNotNumberAndNotEmpty -> result', {
-    result,
-    isInputValueString: isValueString,
-    isInputValeNotNumber: isValueNotNumber,
-  });
-  return result;
-}
-
-function localStorageListOf(key) {
-  const value = localStorage.getItem(key);
-  console.log('> localStorageListOf: value =', value);
-  if (value === null) return [];
-  const parsedValue = JSON.parse(value);
-  const isParsedValueArray = Array.isArray(parsedValue);
-  return isParsedValueArray ? parsedValue : [];
-}
-
-function disableButtonWhenTextInvalid(
-  button,
-  text,
-  validateTextFunction,
-  { textWhenDisabled, textWhenEnabled } = {}
-) {
-  if (!validateTextFunction) throw new Error('Validate method must be defined');
-
-  if (validateTextFunction(text)) {
-    button.disabled = false;
-    if (textWhenEnabled) button.textContent = textWhenEnabled;
-  } else {
-    button.disabled = true;
-    if (textWhenDisabled) button.textContent = textWhenDisabled;
-  }
-}
-
-function renderTodoListInContainer(list, container) {
+function renderTodoListInContainer(listOfTodoVO, container) {
   let output = '';
-  let item;
-  for (let index in list) {
-    item = list[index];
-    output += `<li>${item.title}</li>`;
+  let todoVO;
+  for (let index in listOfTodoVO) {
+    todoVO = listOfTodoVO[index];
+    output += TodoView.createSimpleViewFromVO(index, todoVO);
   }
   container.innerHTML = output;
+
+}
+
+function saveListOfTodo(){
+  localStorageSaveListOfWithKey(LOCAL_LIST_OF_TODOS, listOfTodos);
 }
